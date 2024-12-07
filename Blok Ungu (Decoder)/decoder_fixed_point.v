21da59b73480043c88a3f15bf3efc23ce5ea13d6
Kevin Reagen <kevin.reagen03@gmail.com>
2024-12-07 20:10:27 +0700

    Update decoder_fixed_point.v

--- Blok Ungu (Decoder)/decoder_fixed_point.v
+++ Blok Ungu (Decoder)/decoder_fixed_point.v
@@ -1,21 +1,21 @@
-`include "fixed_point_multiply.v" // Custom multiplier module for our version of 32-bit Fixed_Point
-`include "fixed_point_add.v"      // Custom adder module for our version of 32-bit Fixed_Point
+`include "fixed_point_add.v"
+`include "fixed_point_multiply.v"
 
 module decoder_fixed_point #(
     parameter N_input = 2,        // Jumlah Input
     parameter M_output = 9,       // Jumlah Output
     parameter BITSIZE = 32        // Fixed Point 32-bit
 )(
-    input wire [N_input*BITSIZE-1:0] z,           // Input
-    input wire [N_input*M_output*BITSIZE-1:0] w,  // Weight
-    input wire [M_output*BITSIZE-1:0] b,          // Bias
-    output wire [M_output*BITSIZE-1:0] out        // Output
+    input wire signed [N_input*BITSIZE-1:0] z,           // Input
+    input wire signed [N_input*M_output*BITSIZE-1:0] w,  // Weight
+    input wire signed [M_output*BITSIZE-1:0] b,          // Bias
+    output wire signed [M_output*BITSIZE-1:0] out        // Output
 );
 
     // Internal wires for results
-    wire [BITSIZE-1:0] mult_result [0:N_input-1][0:M_output-1];    // Hasil perkalian
-    wire [BITSIZE-1:0] final_result [0:M_output-1];                // Hasil akhir setelah ditambahkan bias
-    wire [BITSIZE-1:0] tree_sum_result [0:M_output-1];             // Hasil adder tree untuk setiap output
+    wire signed [BITSIZE-1:0] mult_result [0:N_input-1][0:M_output-1];    // Hasil perkalian
+    wire signed [BITSIZE-1:0] final_result [0:M_output-1];                // Hasil akhir setelah ditambahkan bias
+    wire signed [BITSIZE-1:0] tree_sum_result [0:M_output-1];             // Hasil adder tree untuk setiap output
 
     genvar i, j;
 
@@ -24,9 +24,9 @@ module decoder_fixed_point #(
         for (i = 0; i < N_input; i = i + 1) begin : gen_z
             for (j = 0; j < M_output; j = j + 1) begin : gen_w
                 fixed_point_multiply mult_inst (
-                    .A(z[(i+1)*BITSIZE-1:i*BITSIZE]),                             // Input z
-                    .B(w[(j*N_input + i + 1)*BITSIZE-1:(j*N_input + i)*BITSIZE]), // Weight w
-                    .C(mult_result[i][j])                                         // Hasil perkalian
+                    .A(z[(i+1)*BITSIZE-1:i*BITSIZE]),  // Input z: Select the correct slice for z
+                    .B(w[(j*N_input + i)*BITSIZE +: BITSIZE]), // Weight w: Select the correct slice for w
+                    .C(mult_result[i][j]) // Hasil perkalian
                 );
             end
         end
