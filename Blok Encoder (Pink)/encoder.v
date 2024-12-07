1fcd9919cf4d1d0a7737ba2937e17661ef76c864
vannyalviola <82189230+vannyalviola@users.noreply.github.com>
2024-12-07 13:03:36 +0700

    Update encoder.v

--- Blok Encoder (Pink)/encoder.v
+++ Blok Encoder (Pink)/encoder.v
@@ -1,9 +1,10 @@
-`include "multiplier.v" // Include modul multiplier custom
+`include "fixed_point_multiply.v" // Modul custom multiplier
+`include "fixed_point_add.v"      // Modul custom adder
 
-module encoder #(
+module encoder_fixed_point #(
     parameter N_input = 9,       // Jumlah input
     parameter M_output = 4,     // Jumlah output
-    parameter BITSIZE = 32      // Ukuran data (bit)
+    parameter BITSIZE = 32      // Ukuran data (fixed-point 32-bit)
 )(
     input wire [N_input*BITSIZE-1:0] x,           // Input data (flatten)
     input wire [N_input*M_output*BITSIZE-1:0] w, // Bobot (weights, flatten)
@@ -16,6 +17,7 @@ module encoder #(
     // Internal wires untuk hasil perkalian dan penjumlahan
     wire [BITSIZE-1:0] mult_result [0:N_input-1][0:M_output-1]; // Hasil perkalian
     reg [BITSIZE-1:0] sum [0:M_output-1];                      // Penjumlahan sementara
+    wire [BITSIZE-1:0] add_result;                             // Hasil penjumlahan
 
     genvar i, j;
 
@@ -23,12 +25,10 @@ module encoder #(
     generate
         for (i = 0; i < N_input; i = i + 1) begin : gen_x
             for (j = 0; j < M_output; j = j + 1) begin : gen_w
-                multiplier #(
-                    .BITSIZE(BITSIZE)
-                ) mult_inst (
-                    .a(x[(i+1)*BITSIZE-1:i*BITSIZE]),   // Input x
-                    .b(w[(j*N_input + i + 1)*BITSIZE-1:(j*N_input + i)*BITSIZE]), // Input w
-                    .result(mult_result[i][j])          // Hasil perkalian
+                fixed_point_multiply mult_inst (
+                    .A(x[(i+1)*BITSIZE-1:i*BITSIZE]),   // Input x
+                    .B(w[(j*N_input + i + 1)*BITSIZE-1:(j*N_input + i)*BITSIZE]), // Input w
+                    .C(mult_result[i][j])              // Hasil perkalian
                 );
             end
         end
@@ -41,12 +41,22 @@ module encoder #(
             output <= {M_output*BITSIZE{1'b0}};
         end else begin
             for (j = 0; j < M_output; j = j + 1) begin
-                sum[j] = 0; // Reset penjumlahan sementara
+                sum[j] = {BITSIZE{1'b0}}; // Reset penjumlahan sementara
                 for (i = 0; i < N_input; i = i + 1) begin
-                    sum[j] = sum[j] + mult_result[i][j]; // Penjumlahan hasil multiplier
+                    // Gunakan fixed_point_add untuk penjumlahan
+                    fixed_point_add adder_inst (
+                        .A(sum[j]),
+                        .B(mult_result[i][j]),
+                        .C(add_result)
+                    );
+                    sum[j] = add_result; // Update sum dengan hasil penjumlahan
                 end
-                // Tambahkan bias dan assign ke output
-                output[(j+1)*BITSIZE-1:j*BITSIZE] = sum[j] + b[(j+1)*BITSIZE-1:j*BITSIZE];
+                // Tambahkan bias menggunakan fixed_point_add
+                fixed_point_add adder_bias (
+                    .A(sum[j]),
+                    .B(b[(j+1)*BITSIZE-1:j*BITSIZE]),
+                    .C(output[(j+1)*BITSIZE-1:j*BITSIZE])
+                );
             end
         end
     end
