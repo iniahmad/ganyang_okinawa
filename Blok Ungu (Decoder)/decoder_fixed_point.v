15d2230aa8c73045ebe1f2201f3de73e2c86b784
Kevin Reagen <kevin.reagen03@gmail.com>
2024-12-07 17:48:47 +0700

    Update decoder_fixed_point.v

--- Blok Ungu (Decoder)/decoder_fixed_point.v
+++ Blok Ungu (Decoder)/decoder_fixed_point.v
@@ -13,9 +13,9 @@ module decoder_fixed_point #(
 );
 
     // Internal wires for results
-    wire [BITSIZE-1:0] mult_result [0:N_input-1][0:M_output-1];    // Hasil perkalian
+    wire [BITSIZE-1:0] mult_result [0:N_input-1][0:M_output-1];      // Hasil perkalian
     wire [BITSIZE-1:0] final_result [0:M_output-1];                  // Hasil akhir setelah ditambahkan bias
-    wire [BITSIZE-1:0] tree_sum_result [0:M_output-1];              // Hasil adder tree untuk setiap output
+    wire [BITSIZE-1:0] tree_sum_result [0:M_output-1];               // Hasil adder tree untuk setiap output
 
     genvar i, j;
 
@@ -36,8 +36,7 @@ module decoder_fixed_point #(
     generate
         for (j = 0; j < M_output; j = j + 1) begin : gen_adder_tree
             wire [BITSIZE-1:0] level1_sum;
-
-            // Directly sum the two multiplication results in 1 level
+            
             fixed_point_add level1_adder
             (
                 .A(mult_result[0][j]),
@@ -45,17 +44,17 @@ module decoder_fixed_point #(
                 .C(level1_sum)
             );
 
-            assign tree_sum_result[j] = level1_sum; // Assign the summed result to the final result
+            assign tree_sum_result[j] = level1_sum;
         end
     endgenerate
 
-    // Add Bias in Parallel
+    // Penjumlahan Bias Paralel
     generate
         for (j = 0; j < M_output; j = j + 1) begin : gen_bias
             fixed_point_add adder_bias (
-                .A(tree_sum_result[j]),                      // The summed multiplication result
-                .B(b[(j+1)*BITSIZE-1:j*BITSIZE]),              // Bias
-                .C(final_result[j])                              // Final result after adding bias
+                .A(tree_sum_result[j]),                     
+                .B(b[(j+1)*BITSIZE-1:j*BITSIZE]),           
+                .C(final_result[j])                              
             );
             assign out[(j+1)*BITSIZE-1:j*BITSIZE] = final_result[j];
         end
